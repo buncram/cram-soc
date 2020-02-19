5bbb66ba3484ea66f943b1a0153e855643cbbaab
bunnie <bunnie@kosagi.com>
2020-02-20 03:03:19 +0800

    add csr.csv copy to target

--- sw/buildpush.sh
+++ sw/buildpush.sh
@@ -13,9 +13,9 @@ fi
 # case of no private key specified
 if [ -z "$2" ]
 then
-cargo +nightly build --release && ./rust-rom.sh && scp /tmp/betrusted-soc.bin $1:code/betrusted-scripts/ && scp ../build/gateware/encrypted.bin $1:code/betrusted-scripts/
+cargo +nightly build --release && ./rust-rom.sh && scp /tmp/betrusted-soc.bin $1:code/betrusted-scripts/ && scp ../build/gateware/encrypted.bin $1:code/betrusted-scripts/ && scp ../test/csr.csv $1:code/betrusted-scripts/soc-csr.csv
 else
 # there is a private key
-cargo +nightly build --release && ./rust-rom.sh && scp -i $2 /tmp/betrusted-soc.bin $1:code/betrusted-scripts/ && scp -i $2 ../build/gateware/encrypted.bin $1:code/betrusted-scripts/
+cargo +nightly build --release && ./rust-rom.sh && scp -i $2 /tmp/betrusted-soc.bin $1:code/betrusted-scripts/ && scp -i $2 ../build/gateware/encrypted.bin $1:code/betrusted-scripts/ && scp -i $2 ../test/csr.csv $1:code/betrusted-scripts/soc-csr.csv
 fi
 
