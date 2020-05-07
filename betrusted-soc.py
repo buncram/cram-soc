59ef8446ac4224135af88fabc3daea6b0ea749a3
bunnie <bunnie@kosagi.com>
2020-05-07 15:07:03 +0800

    move AES to 50MHz clock domain
    timing can't close at 100MHz based on new features pulled
    into the openTitan core since last merge

--- betrusted-soc.py
+++ betrusted-soc.py
@@ -845,46 +845,99 @@ class Aes(Module, AutoDoc, AutoCSR):
             CSRField("data_out_clear", size=1, description="Clears the data output"),
             CSRField("prng_reseed", size=1, description="Reseed PRNG"),
         ])
+        key0re50 = Signal()
+        self.submodules.key0re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key0re.i.eq(self.key_0_q.re), key0re50.eq(self.key0re.o)]
+        key1re50 = Signal()
+        self.submodules.key1re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key1re.i.eq(self.key_1_q.re), key1re50.eq(self.key1re.o)]
+        key2re50 = Signal()
+        self.submodules.key2re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key2re.i.eq(self.key_2_q.re), key2re50.eq(self.key2re.o)]
+        key3re50 = Signal()
+        self.submodules.key3re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key3re.i.eq(self.key_3_q.re), key3re50.eq(self.key3re.o)]
+        key4re50 = Signal()
+        self.submodules.key4re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key4re.i.eq(self.key_4_q.re), key4re50.eq(self.key4re.o)]
+        key5re50 = Signal()
+        self.submodules.key5re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key5re.i.eq(self.key_5_q.re), key5re50.eq(self.key5re.o)]
+        key6re50 = Signal()
+        self.submodules.key6re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key6re.i.eq(self.key_6_q.re), key6re50.eq(self.key6re.o)]
+        key7re50 = Signal()
+        self.submodules.key7re = BlindTransfer("sys", "clk50")
+        self.comb += [self.key7re.i.eq(self.key_7_q.re), key0re50.eq(self.key7re.o)]
+
+        dataout0_50 = Signal()
+        self.submodules.dataout0_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.dataout0_50.i.eq(self.dataout_0.we), dataout0_50.eq(self.dataout0_50.o)]
+        dataout1_50 = Signal()
+        self.submodules.dataout1_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.dataout1_50.i.eq(self.dataout_1.we), dataout1_50.eq(self.dataout1_50.o)]
+        dataout2_50 = Signal()
+        self.submodules.dataout2_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.dataout2_50.i.eq(self.dataout_2.we), dataout2_50.eq(self.dataout2_50.o)]
+        dataout3_50 = Signal()
+        self.submodules.dataout3_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.dataout3_50.i.eq(self.dataout_3.we), dataout3_50.eq(self.dataout3_50.o)]
+
+        iv0_50 = Signal()
+        self.submodules.iv0_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.iv0_50.i.eq(self.iv_0.re), iv0_50.eq(self.iv0_50.o)]
+        iv1_50 = Signal()
+        self.submodules.iv1_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.iv1_50.i.eq(self.iv_1.re), iv1_50.eq(self.iv1_50.o)]
+        iv2_50 = Signal()
+        self.submodules.iv2_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.iv2_50.i.eq(self.iv_2.re), iv2_50.eq(self.iv2_50.o)]
+        iv3_50 = Signal()
+        self.submodules.iv3_50 = BlindTransfer("sys", "clk50")
+        self.comb += [self.iv3_50.i.eq(self.iv_3.re), iv3_50.eq(self.iv3_50.o)]
+
+        ctrlre50 = Signal()
+        self.submodules.ctrl50re = BlindTransfer("sys", "clk50")
+        self.comb += [self.ctrl50re.i.eq(self.ctrl.re), ctrlre50.eq(self.ctrl50re.o)]
 
         self.specials += Instance("aes_reg_top",
-            i_clk_i = ClockSignal(),
-            i_rst_ni = ~ResetSignal(),
+            i_clk_i = ClockSignal("clk50"),
+            i_rst_ni = ~ResetSignal("clk50"),
 
-            # TODO implement key clearing?
             i_key_0_q=self.key_0_q.fields.key_0,
-            i_key_0_qe=self.key_0_q.re,
+            i_key_0_qe=key0re50,
             i_key_1_q=self.key_1_q.fields.key_1,
-            i_key_1_qe=self.key_1_q.re,
+            i_key_1_qe=key1re50,
             i_key_2_q=self.key_2_q.fields.key_2,
-            i_key_2_qe=self.key_2_q.re,
+            i_key_2_qe=key2re50,
             i_key_3_q=self.key_3_q.fields.key_3,
-            i_key_3_qe=self.key_3_q.re,
+            i_key_3_qe=key3re50,
             i_key_4_q=self.key_4_q.fields.key_4,
-            i_key_4_qe=self.key_4_q.re,
+            i_key_4_qe=key4re50,
             i_key_5_q=self.key_5_q.fields.key_5,
-            i_key_5_qe=self.key_5_q.re,
+            i_key_5_qe=key5re50,
             i_key_6_q=self.key_6_q.fields.key_6,
-            i_key_6_qe=self.key_6_q.re,
+            i_key_6_qe=key6re50,
             i_key_7_q=self.key_7_q.fields.key_7,
-            i_key_7_qe=self.key_7_q.re,
+            i_key_7_qe=key7re50,
 
             o_data_out_0=self.dataout_0.fields.data,
-            i_data_out_0_re=self.dataout_0.we,
+            i_data_out_0_re=dataout0_50,
             o_data_out_1=self.dataout_1.fields.data,
-            i_data_out_1_re=self.dataout_1.we,
+            i_data_out_1_re=dataout1_50,
             o_data_out_2=self.dataout_2.fields.data,
-            i_data_out_2_re=self.dataout_2.we,
+            i_data_out_2_re=dataout2_50,
             o_data_out_3=self.dataout_3.fields.data,
-            i_data_out_3_re=self.dataout_3.we,
+            i_data_out_3_re=dataout3_50,
 
             i_iv_0_q=self.iv_0.fields.iv,
-            i_iv_0_qe=self.iv_0.re,
+            i_iv_0_qe=iv0_50,
             i_iv_1_q=self.iv_1.fields.iv,
-            i_iv_1_qe=self.iv_1.re,
+            i_iv_1_qe=iv1_50,
             i_iv_2_q=self.iv_2.fields.iv,
-            i_iv_2_qe=self.iv_2.re,
+            i_iv_2_qe=iv2_50,
             i_iv_3_q=self.iv_3.fields.iv,
-            i_iv_3_qe=self.iv_3.re,
+            i_iv_3_qe=iv3_50,
 
             i_data_in_0_to_core=self.datain_0.fields.data,
             i_data_in_1_to_core=self.datain_1.fields.data,
@@ -900,7 +953,7 @@ class Aes(Module, AutoDoc, AutoCSR):
             o_ctrl_key_len_rbk=self.status.fields.key_len_rbk,
             i_ctrl_manual_operation=self.ctrl.fields.manual_operation,
             i_ctrl_operation=self.ctrl.fields.operation,
-            i_ctrl_update=self.ctrl.re,
+            i_ctrl_update=ctrlre50,
 
             o_idle=self.status.fields.idle,
             o_stall=self.status.fields.stall,
