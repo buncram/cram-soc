172c5fe8a87af7ed1483f2105910ab65c30bdb00
bunnie <bunnie@kosagi.com>
2020-06-29 19:13:54 +0800

    helper script to harvest random numbers for analysis

---
+++ rng_helper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 [filename_root] <iterations>"
+    echo "If no iterations specified, a single iteration is assumed"
+    exit 1
+fi
+
+if [ "$#" -eq 2 ]; then
+    ITERATIONS=$2
+    echo "Running $ITERATIONS of dump..."
+else
+    ITERATIONS=1
+fi
+
+i=0
+
+while [ $i -le $ITERATIONS ]
+	 do
+	     SUFFIX=$(date +'%s')
+	     NAME=$1_$SUFFIX
+	     echo "Dumping 8MiB memory to $NAME"
+	     yes | riscv64-unknown-elf-gdb -ex 'file /home/bunnie/code/betrusted-soc/fw/target/riscv32imac-unknown-none-elf/release/betrusted-soc' -ex 'target remote 10.0.245.90:3333' -ex 'mon reset' -ex 'quit'
+	     # takes 36 seconds to run the generator on boot
+	     # sleep 40
+	     sleep 4
+	     yes | riscv64-unknown-elf-gdb -ex 'file /home/bunnie/code/betrusted-soc/fw/target/riscv32imac-unknown-none-elf/release/betrusted-soc' -ex 'target remote 10.0.245.90:3333' -ex "dump memory $NAME 0x40080000 0x40090000" -ex 'quit'
+	     ((i++))
+done
