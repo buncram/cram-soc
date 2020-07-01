8b042e0a418cfaf9b4cc47a2aae4b4ec5b6c835d
bunnie <bunnie@kosagi.com>
2020-07-01 17:53:16 +0800

    mods for ring oscillator characterization

--- rng_helper.sh
+++ rng_helper.sh
@@ -21,8 +21,10 @@ while [ $i -le $ITERATIONS ]
 	     NAME=$1_$SUFFIX
 	     echo "Iteration $i: Dumping memory to $NAME"
 	     yes | riscv64-unknown-elf-gdb -ex 'file /home/bunnie/code/betrusted-soc/fw/target/riscv32imac-unknown-none-elf/release/betrusted-soc' -ex 'target remote 10.0.245.90:3333' -ex 'mon reset' -ex 'quit'
-	     # takes 36 seconds to run the generator on boot
-	     sleep 40
+	     # takes 36 seconds to run the avalanche generator on boot
+	     # sleep 40
+	     # takes 97 seconds to run the ring oscillator generator on boot
+	     sleep 120  # a bit longer because ro timing is variable with temperature
 	     # sleep 4
 	     yes | riscv64-unknown-elf-gdb -ex 'file /home/bunnie/code/betrusted-soc/fw/target/riscv32imac-unknown-none-elf/release/betrusted-soc' -ex 'target remote 10.0.245.90:3333' -ex "dump memory $NAME 0x40080000 0x40880000" -ex 'quit'
 	     ((i++))
