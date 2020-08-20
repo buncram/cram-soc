494479a8d8f7f9c8edea3ce3ab37e0dc8e1bc6a4
bunnie <bunnie@kosagi.com>
2020-08-21 01:36:43 +0800

    modify double ratchet structure to use hardware accelerator

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -6,7 +6,7 @@
 #![no_std]
 
 #[export_name = "engine_vectors"]
-pub const ENGINE_VECTORS: &[u8; 13220] = include_bytes!("test_vectors.bin");
+pub const ENGINE_VECTORS: &[u8; 15652] = include_bytes!("test_vectors.bin");
 use volatile::Volatile;
 
 use core::panic::PanicInfo;
