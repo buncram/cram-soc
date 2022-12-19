fa8ec0ccb892390357b7c15f804638afeb98bdb7
buncram <cramium@kosagi.com>
2022-12-20 02:01:26 +0800

    add renadem artifacts; pull in satp reveal

--- candidate/stage.sh
+++ candidate/stage.sh
@@ -2,8 +2,8 @@
 
 cp ../sim_support/ram_1w_1ra.v .
 cp ../sim_support/ram_1w_1rs.v .
-cp ../deps/pythondata-cpu-vexriscv/pythondata_cpu_vexriscv/verilog/VexRiscv_CranSoC.v_toplevel_memory_AesPlugin_rom_storage.bin .
-cp ../deps/pythondata-cpu-vexriscv/pythondata_cpu_vexriscv/verilog/VexRiscv_CranSoC.v .
+cp ../deps/pythondata-cpu-vexriscv/pythondata_cpu_vexriscv/verilog/VexRiscv_CramSoC.v_toplevel_memory_AesPlugin_rom_storage.bin .
+cp ../deps/pythondata-cpu-vexriscv/pythondata_cpu_vexriscv/verilog/VexRiscv_CramSoC.v .
 cp ../build/gateware/cram_axi.v .
 
 # copy over the AXI rtl models
