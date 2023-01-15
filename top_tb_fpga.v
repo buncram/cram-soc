599ffe6228fdf266c59dd0baff32d8a0492b3106
buncram <cramium@kosagi.com>
2023-01-16 02:26:51 +0800

    add UART debug printing to simulator log

--- top_tb_fpga.v
+++ top_tb_fpga.v
@@ -1,5 +1,46 @@
 `timescale 1ns/1ps
 
+module uart_print #(
+    parameter TYPE="generic"
+)(
+    input logic [7:0] uart_data,
+    input logic uart_data_valid,
+    input logic resetn,
+    input logic clk
+);
+    // print debug strings
+    `define theregfull( theclk, theresetn, theregname, theinitvalue ) \
+        always@( posedge theclk or negedge theresetn ) \
+        if( ~theresetn) \
+            theregname <= theinitvalue; \
+        else \
+            theregname
+
+    `define theregrn(theregname) \
+        `theregfull( clk, resetn, theregname, '0 )
+
+    localparam CHARLEN = 256;
+    logic                       charbufwr, charbuffill, charbufclr;
+    bit [$clog2(CHARLEN)-1:0]   charbufidx;
+    bit [0:CHARLEN-1][7:0]      charbufdat;
+    string charbufstring;
+    assign charbufwr = uart_data_valid;
+    assign charbuffill = charbufwr & ~(( uart_data[7:0] == 'h0d ) | ( uart_data[7:0] == 'h0a ));
+    assign charbufclr  = charbufwr &  (( uart_data[7:0] == 'h0d ) | ( uart_data[7:0] == 'h0a ));
+
+    `theregrn( charbufidx ) <= charbufclr ? '0 : charbuffill ? charbufidx + 1 : charbufidx;
+    `theregrn( charbufdat[charbufidx] ) <= charbuffill ? uart_data[7:0] : charbufdat[charbufidx];
+
+    always@( negedge clk )
+    if( uart_data_valid )  begin
+        if( charbufclr ) begin
+            charbufstring = string'(charbufdat);
+            $display("[%s] %s", TYPE, charbufstring );
+            charbufdat = '0;
+        end
+    end
+endmodule
+
 module top_tb_fpga();
 
 /////////// boilerplate in here
@@ -76,6 +117,14 @@ wire sram_zz_n;
 wire [31:0] sram_d;
 wire [3:0] sram_dm_n;
 
+wire [7:0] uart_kernel;
+wire uart_kernel_valid;
+wire [7:0] uart_log;
+wire uart_log_valid;
+wire [7:0] uart_app;
+wire uart_app_valid;
+wire clk;
+
 cram_fpga dut (
     .clk12(clk12),
     .lpclk(lpclk),
@@ -107,10 +156,34 @@ cram_fpga dut (
     .lcd_si(si),
     .lcd_scs(scs),
 
+    .sim_uart_kernel(uart_kernel),
+    .sim_uart_kernel_valid(uart_kernel_valid),
+    .sim_uart_log(uart_log),
+    .sim_uart_log_valid(uart_log_valid),
+    .sim_uart_app(uart_app),
+    .sim_uart_app_valid(uart_app_valid),
+    .sim_sysclk(clk),
     .sim_coreuser(coreuser),
     .sim_success(success),
     .sim_done(done),
     .sim_report(report)
+);
+
+uart_print #(
+    .TYPE("kernel")
+) kernel (
+    .uart_data(uart_kernel),
+    .uart_data_valid(uart_kernel_valid),
+    .resetn(~trst),
+    .clk(clk)
+);
+uart_print #(
+    .TYPE("log")
+) log (
+    .uart_data(uart_log),
+    .uart_data_valid(uart_log_valid),
+    .resetn(~trst),
+    .clk(clk)
 );
 
 // extra reporting for CI
@@ -118,13 +191,12 @@ initial begin
         $dumpvars(0, sclk);
         $dumpvars(0, si);
         $dumpvars(0, scs);
-        $dumpvars(0, serial_tx);
-        $dumpvars(0, serial_rx);
-        $dumpvars(0, tck);
-        $dumpvars(0, tms);
-        $dumpvars(0, tdi);
-        $dumpvars(0, tdo);
-        $dumpvars(0, trst);
+        $dumpvars(0, uart_kernel);
+        $dumpvars(0, uart_kernel_valid);
+        $dumpvars(0, uart_log);
+        $dumpvars(0, uart_log_valid);
+        $dumpvars(0, uart_app);
+        $dumpvars(0, uart_app_valid);
 end
 
 // DUT-specific end condition to make sure it eventually stops running for CI mode
