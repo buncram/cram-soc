3991709003c462f149d12064d24388922905f97d
buncram <cramium@kosagi.com>
2023-02-11 18:44:52 +0800

    add support for kernel + binary zooming

--- codezoom.py
+++ codezoom.py
@@ -13,14 +13,29 @@ def main(stdscr):
         "--file", required=False, help="The file to zoom around", type=str, default="../xous-cramium/load.lst"
     )
     parser.add_argument(
+        "--kernel", required=False, help="The kernel to zoom around", type=str, default="../xous-cramium/kernel.lst"
+    )
+    parser.add_argument(
         "--port", required=False, help="The port to listen on", type=int, default=6502
     )
     args = parser.parse_args()
+
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    stdscr.nodelay(True)
+
     udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", args.port))
 
+    files = {}
     with open(args.file, 'r') as file_handle:
         text = file_handle.readlines()
+        files['user'] = text
+
+    with open(args.kernel, 'r') as kernel_handle:
+        kernel = kernel_handle.readlines()
+        files['kernel'] = kernel
 
     rows, cols = stdscr.getmaxyx()
 
@@ -36,9 +51,20 @@ def main(stdscr):
             if data[0] == 2: # value type
                 strlen = data[1]
                 string = data[2:2+strlen].decode('utf-8')
+                try:
+                    offset = int(string, 16)
+                    if offset >= 0xfd00_0000:
+                        region = 'kernel'
+                    else:
+                        region = 'user'
+                    string = hex(offset).lstrip('0x')
+                except ValueError:
+                    pass
+
+                text = files[region]
                 # now find the line number that contains this string
                 for index, line in enumerate(text):
-                    if line.lower().startswith(string.lower()):
+                    if line.lower().lstrip().startswith(string.lower()):
                         text_offset = index
                         break
                 # center the line number on the screen
@@ -48,7 +74,7 @@ def main(stdscr):
                     start_line = 0
 
                 stdscr.clear()
-                for i in range(rows):
+                for i in range(rows - 1):
                     if start_line + i == text_offset:
                         if start_line + i < len(text):
                             try:
@@ -61,6 +87,7 @@ def main(stdscr):
                                 stdscr.addstr(i, 0, text[start_line + i].rstrip())
                             except:
                                 pass
+                stdscr.addstr(rows - 1, 0, "Region: {}".format(region), curses.A_REVERSE)
 
         stdscr.refresh()
         try:
@@ -72,11 +99,6 @@ def main(stdscr):
 
 if __name__ == "__main__":
     stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(True)
-    stdscr.nodelay(True)
-
     main(stdscr)
 
     stdscr.nodelay(False)
