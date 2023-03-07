d2b400fae616c22f98d58e6c07e092fbc1149f15
buncram <cramium@kosagi.com>
2023-03-08 03:43:09 +0800

    add kernel uart output

--- sim_support/finisher.v
+++ sim_support/finisher.v
@@ -4,12 +4,21 @@
 // particularly around reset conditions.
 
 module finisher(
+    input wire [7:0] kuart_from_cpu,
+    input wire kuart_from_cpu_valid,
+    output wire [7:0] kuart_to_cpu,
+    output reg kuart_to_cpu_valid,
+    input wire kuart_to_cpu_ready,
     input wire [31:0] report,
     input wire success,
-    input wire done
+    input wire done,
+    input wire clk
 );
 
 reg [31:0] rep_state;
+reg kprint_trigger = 0;
+reg kprint_trigger_d = 0;
+reg kprint_done = 0;
 
 always @(report) begin
     if (report != rep_state) begin
@@ -19,7 +28,7 @@ always @(report) begin
 end
 
 always @(*) begin
-    if (done == 1'b1) begin
+    if (kprint_done == 1'b1) begin
         if (success == 1'b1) begin
             $display("Simulation success: %h", report);
         end else begin
@@ -28,6 +37,38 @@ always @(*) begin
         $dumpflush;
         $dumpoff;
         $finish;
+    end
+end
+
+// just one character is needed for the ram print trigger
+assign kuart_to_cpu = 8'h72; // 'r'
+
+// This block waits for `done` to rise; then, it triggers an 'r' into the kernel
+// so that the RAM usage dump gets printed. Finally, the simulation termination
+// signal `kprint_done` is triggered when the '.' character is seen in the kernel log.
+always @(posedge clk) begin
+    // make the trigger "sticky" and one-way
+    if (done == 1'b1) begin
+        kprint_trigger <= 1'b1;
+    end else begin
+        kprint_trigger <= kprint_trigger;
+    end
+
+    // extract the rising edge, if we see it, send the 'r' character by triggering valid
+    // we assume the "ready" is always ready...
+    kprint_trigger_d <= kprint_trigger;
+    if (kprint_trigger & !kprint_trigger_d) begin
+        $display("Got end of simulation signal; triggering kernel RAM usage print");
+        kuart_to_cpu_valid <= 1'b1;
+    end else begin
+        kuart_to_cpu_valid <= 1'b0;
+    end
+
+    // signal simulation end when the final trailing '.' is seen, but only after the trigger is set
+    if ((kprint_trigger == 1'b1) && (kuart_from_cpu == 8'h2e /* '.' */) && (kuart_from_cpu_valid == 1'b1)) begin
+        kprint_done <= 1'b1;
+    end else begin
+        kprint_done <= kprint_done;
     end
 end
 
