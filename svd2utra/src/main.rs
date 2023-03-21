3e396075ffa88b910fd76e5f7be07da78ea26e32
buncram <cramium@kosagi.com>
2023-03-21 15:54:51 +0800

    add svd2utra lib

---
+++ svd2utra/src/main.rs
@@ -0,0 +1,17 @@
+mod generate;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if std::env::args().count() != 3 {
+        println!("Usage: svd2utra <input SVD> <output utra>");
+        return Ok(())
+    }
+    let svd_filename = std::env::args().nth(1).ok_or("Must specify SVD input filename")?;
+    let generated_filename = std::env::args().nth(2).ok_or("Must specify destination utralib filename")?;
+
+    let src_file = std::fs::File::open(svd_filename).expect("couldn't open src file");
+    let mut dest_file = std::fs::File::create(generated_filename).expect("couldn't open dest file");
+
+    generate::generate(src_file, &mut dest_file)?;
+
+    Ok(())
+}
