68a4f465d503dbd7764dbb6a0efd3e76070f7494
buncram <cramium@kosagi.com>
2023-05-12 04:47:36 +0800

    add NEC loopback test
    just a raw port, still needs tuning

--- boot/betrusted-boot/src/pio_tests/mod.rs
+++ boot/betrusted-boot/src/pio_tests/mod.rs
@@ -2,8 +2,10 @@ pub mod i2c;
 pub mod spi;
 pub mod units;
 pub mod adder;
+pub mod nec;
 
 pub fn pio_tests() {
+    // nec::nec_ir_loopback_test();
     units::register_tests();
     units::restart_imm_test();
     units::fifo_join_test();
