2576dd83059fac2fc17023ef8dc339a24198963c
buncram <cramium@kosagi.com>
2023-08-08 23:06:34 +0800

    cleanup some scripts

--- boot/daric.sh
+++
@@ -1,6 +0,0 @@
-cp betrusted-boot/link.x.straight betrusted-boot/link.x # to be used in conjunction with --feature gdb-load
-cargo xtask boot-image --no-default-features --feature daric --feature full-chip --feature gdb-load --feature pl230-test # --feature apb-test # --feature pio-test
-
-riscv-none-elf-objdump -h target/riscv32imac-unknown-none-elf/release/betrusted-boot > boot.lst
-riscv-none-elf-nm -r --size-sort --print-size target/riscv32imac-unknown-none-elf/release/betrusted-boot | rustfilt >> boot.lst
-riscv-none-elf-objdump target/riscv32imac-unknown-none-elf/release/betrusted-boot -S -d | rustfilt >> boot.lst
