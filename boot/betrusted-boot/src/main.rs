cdc1e332bf24c3c44f0593c41ebe03456ecb2577
buncram <cramium@kosagi.com>
2023-09-15 11:24:26 +0800

    add FPGA target for MMCM clock adjustments

--- boot/betrusted-boot/src/main.rs
+++ boot/betrusted-boot/src/main.rs
@@ -354,15 +354,18 @@ pub fn snap_ticks(title: &str) {
     uart.tiny_write_str(" ticks\n");
 }
 
+
 #[cfg(feature="full-chip")]
 pub fn early_init() {
     let mut uart = debug::Uart {};
 
+    #[cfg(not(feature="cram-fpga"))]
     unsafe {
         (0x400400a0 as *mut u32).write_volatile(0x1F598); // F
         uart.print_hex_word((0x400400a0 as *const u32).read_volatile());
         uart.putc('\n' as u32 as u8);
         let poke_array: [(u32, u32, bool); 12] = [
+            // commented out because the FPGA does not take kindly to this being set twice
             (0x400400a4, 0x2812, false),   //  MN
             (0x400400a8, 0x3301, false),   //  Q
             (0x40040090, 0x0032, true),  // setpll
@@ -391,6 +394,35 @@ pub fn early_init() {
             uart.putc('\n' as u32 as u8);
         }
     }
+    #[cfg(feature="cram-fpga")]
+    unsafe {
+        let poke_array: [(u32, u32, bool); 9] = [
+            (0x40040030, 0x0001, true),  // cgusel1
+            (0x40040010, 0x0001, true),  // cgusel0
+            (0x40040010, 0x0001, true),  // cgusel0
+            (0x40040014, 0x007f, true),  // fdfclk
+            (0x40040018, 0x007f, true),  // fdaclk
+            (0x4004001c, 0x007f, true),  // fdhclk
+            (0x40040020, 0x007f, true),  // fdiclk
+            (0x40040024, 0x007f, true),  // fdpclk
+            (0x400400a0, 0x4040, false),  // pllmn FPGA
+        ];
+        for &(addr, dat, is_u32) in poke_array.iter() {
+            let rbk = if is_u32 {
+                (addr as *mut u32).write_volatile(dat);
+                (addr as *const u32).read_volatile()
+            } else {
+                (addr as *mut u16).write_volatile(dat as u16);
+                (addr as *const u16).read_volatile() as u32
+            };
+            uart.print_hex_word(rbk);
+            if dat != rbk {
+                uart.putc('*' as u32 as u8);
+            }
+            uart.putc('\n' as u32 as u8);
+        }
+    }
+
 }
 
 // these register do not exist in our local simulation model
@@ -417,11 +449,11 @@ pub fn setup_uart1() {
     // TODO: fix parameter resolution to trace back to the top level file. there's actually 6*16 IO banks, not the 4 specified in the leaf design file :P
     let iox_csr = utra::iox::HW_IOX_BASE as *mut u32;
     unsafe {
-        iox_csr.add(0).write_volatile(0x0140);  // PAL
+        iox_csr.add(0).write_volatile(0b00_00_00_01_01_00_00_00);  // PAL AF1 on PA3/PA4
         iox_csr.add(0x1c / core::mem::size_of::<u32>()).write_volatile(0x1400); // PDH
-        iox_csr.add(0x148 / core::mem::size_of::<u32>()).write_volatile(0xff); // PA
+        iox_csr.add(0x148 / core::mem::size_of::<u32>()).write_volatile(0x10); // PA4 output
         iox_csr.add(0x148 / core::mem::size_of::<u32>() + 3).write_volatile(0xffff); // PD
-        iox_csr.add(0x160 / core::mem::size_of::<u32>()).write_volatile(0xffff); // pullups for port A
+        iox_csr.add(0x160 / core::mem::size_of::<u32>()).write_volatile(0x8); // PA3 pullup
     }
 
     // TODO: fix register generator, input is incorrect and pins peripheral count at 6
@@ -436,18 +468,18 @@ pub fn setup_uart1() {
     udma_uart.wo(utra::udma_uart_0::REG_UART_SETUP,
         0x0306 | (clk_counter << 16));
 
-    //for i in 0..16 {
-        let tx_buf = utralib::HW_IFRAM0_MEM as *mut u8;
-        // let mut tx_buf = [0u8; 256];
-        unsafe { tx_buf.write_volatile('0' as u32 as u8 /* + i */) };
-        udma_uart.wo(utra::udma_uart_0::REG_TX_SADDR, tx_buf as u32);
-        udma_uart.wo(utra::udma_uart_0::REG_TX_SIZE, 1);
-        // send it
-        udma_uart.wo(utra::udma_uart_0::REG_TX_CFG, 0x10); // EN
-        // wait for it all to be done
-        while udma_uart.rf(utra::udma_uart_0::REG_TX_CFG_R_TX_EN) != 0 {   }
-        while (udma_uart.r(utra::udma_uart_0::REG_STATUS) & 1) != 0 {  }
-    //}
+    let tx_buf = utralib::HW_IFRAM0_MEM as *mut u8;
+    // let mut tx_buf = [0u8; 256];
+    for i in 0..16 {
+        unsafe { tx_buf.add(i).write_volatile('0' as u32 as u8 + i as u8) };
+    }
+    udma_uart.wo(utra::udma_uart_0::REG_TX_SADDR, tx_buf as u32);
+    udma_uart.wo(utra::udma_uart_0::REG_TX_SIZE, 2);
+    // send it
+    udma_uart.wo(utra::udma_uart_0::REG_TX_CFG, 0x10); // EN
+    // wait for it all to be done
+    while udma_uart.rf(utra::udma_uart_0::REG_TX_CFG_R_TX_EN) != 0 {   }
+    while (udma_uart.r(utra::udma_uart_0::REG_STATUS) & 1) != 0 {  }
     uart.tiny_write_str("udma done\r");
 
 }
@@ -503,26 +535,42 @@ pub unsafe extern "C" fn rust_entry(_unused1: *const usize, _unused2: u32) -> ! 
         let mut uart = debug::Uart {};
         uart.tiny_write_str("hello world!\r");
     }
-
-    // ---------- pio test option -------------
     #[cfg(feature="pio-test")]
     xous_pio::pio_tests::setup_reporting((utra::main::REPORT.offset() + utra::main::HW_MAIN_BASE) as *mut u32);
+
+    // ---------- PIO hack-test ----------------
     #[cfg(feature="pio-test")]
-    xous_pio::pio_tests::pio_tests();
+    {
+        uart.tiny_write_str("spi test\r");
+        pio_hack_test();
+        uart.tiny_write_str("spi test done\r");
+    }
+
+    // ---------- pio test option -------------
+    //#[cfg(feature="pio-test")]
+    //xous_pio::pio_tests::pio_tests();
 
     // ---------- exception setup ------------------
     irqs::irq_setup();
 
     // ---------- PL230 test option ----------------
-    #[cfg(feature="pl230-test")]
-    xous_pl230::pl230_tests::pl230_tests();
+    #[cfg(feature="pl230-test")] {
+        let iox_csr = utra::iox::HW_IOX_BASE as *mut u32;
+        unsafe {
+            iox_csr.add(0x8 / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PBL
+            iox_csr.add(0xC / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PBH
+            iox_csr.add(0x10 / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PCL
+            iox_csr.add(0x14 / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PCH
+            iox_csr.add(0x200 / core::mem::size_of::<u32>()).write_volatile(0xffffffff); // PIO sel port D31-0
+        }
+        xous_pl230::pl230_tests::pl230_tests();
+    }
 
     uart.tiny_write_str("done\r");
-    loop {
-    }
 
     // ---------- coreuser test --------------------
     satp::satp_test();
+    uart.tiny_write_str("satp done\r");
 
     // ---------- exception test -------------------
     irqs::irq_test();
@@ -685,3 +733,129 @@ pub unsafe extern "C" fn rust_entry(_unused1: *const usize, _unused2: u32) -> ! 
     }
 }
 
+use xous_pio::*;
+use xous_pio::pio_tests::spi::*;
+
+pub fn spi_test_core_boot(pio_sm: &mut PioSm) -> bool {
+    report_api(0x0D10_05D1);
+
+    const BUF_SIZE: usize = 20;
+    let mut state: u16 = 0xAF;
+    let mut tx_buf = [0u8; BUF_SIZE];
+    let mut rx_buf = [0u8; BUF_SIZE];
+    // init the TX buf
+    for d in tx_buf.iter_mut() {
+        state = crate::lfsr_next(state);
+        *d = state as u8;
+        report_api(*d as u32);
+    }
+    pio_spi_write8_read8_blocking(pio_sm, &tx_buf, &mut rx_buf);
+    let mut pass = true;
+    for (&s, &d) in tx_buf.iter().zip(rx_buf.iter()) {
+        if s != d {
+            report_api(0xDEAD_0000 | (s as u32) << 8 | ((d as u32) << 0));
+            pass = false;
+        }
+    }
+    report_api(0x600D_05D1);
+    pass
+}
+
+pub fn pio_hack_test() -> bool {
+    let iox_csr = utra::iox::HW_IOX_BASE as *mut u32;
+    unsafe {
+        iox_csr.add(0x8 / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PBL
+        iox_csr.add(0xC / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PBH
+        iox_csr.add(0x10 / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PCL
+        iox_csr.add(0x14 / core::mem::size_of::<u32>()).write_volatile(0b0101_0101_0101_0101);  // PCH
+        iox_csr.add(0x200 / core::mem::size_of::<u32>()).write_volatile(0xffffffff); // PIO sel port D31-0
+    }
+
+    const PIN_SCK: usize = 16;  // PC00
+    const PIN_MOSI: usize = 17; // PC01
+    const PIN_MISO: usize = 17; // loopback    18; // PC02
+
+    let mut pio_csr = CSR::new(utra::rp_pio::HW_RP_PIO_BASE as *mut u32);
+
+    report_api(0x0D10_05D1);
+
+    let mut pio_ss = PioSharedState::new();
+    let mut pio_sm = pio_ss.alloc_sm().unwrap();
+
+    // spi_cpha0 example
+    let spi_cpha0_prog = pio_proc::pio_asm!(
+        ".side_set 1",
+        "out pins, 1 side 0 [1]",
+        "in pins, 1  side 1 [1]",
+    );
+    // spi_cpha1 example
+    let spi_cpha1_prog = pio_proc::pio_asm!(
+        ".side_set 1",
+        "out x, 1    side 0", // Stall here on empty (keep SCK deasserted)
+        "mov pins, x side 1 [1]", // Output data, assert SCK (mov pins uses OUT mapping)
+        "in pins, 1  side 0" // Input data, deassert SCK
+    );
+    let prog_cpha0 = LoadedProg::load(spi_cpha0_prog.program, &mut pio_ss).unwrap();
+    report_api(0x05D1_0000);
+    let prog_cpha1 = LoadedProg::load(spi_cpha1_prog.program, &mut pio_ss).unwrap();
+    report_api(0x05D1_0001);
+
+    let clkdiv: f32 = 137.25;
+    let mut passing = true;
+    let mut cpol = false;
+    pio_csr.wo(utra::rp_pio::SFR_IRQ0_INTE, pio_sm.sm_bitmask());
+    pio_csr.wo(utra::rp_pio::SFR_IRQ1_INTE, (pio_sm.sm_bitmask()) << 4);
+    loop {
+        // pha = 1
+        report_api(0x05D1_0002);
+        pio_spi_init(
+            &mut pio_sm,
+            &prog_cpha0, // cpha set here
+            8,
+            clkdiv,
+            cpol,
+            PIN_SCK,
+            PIN_MOSI,
+            PIN_MISO
+        );
+        report_api(0x05D1_0003);
+        if spi_test_core_boot(&mut pio_sm) == false {
+            passing = false;
+        };
+
+        // pha = 0
+        report_api(0x05D1_0004);
+        pio_spi_init(
+            &mut pio_sm,
+            &prog_cpha1, // cpha set here
+            8,
+            clkdiv,
+            cpol,
+            PIN_SCK,
+            PIN_MOSI,
+            PIN_MISO
+        );
+        report_api(0x05D1_0005);
+        if spi_test_core_boot(&mut pio_sm) == false {
+            passing = false;
+        };
+        if cpol {
+            break;
+        }
+        // switch to next cpol value for test
+        cpol = true;
+    }
+    // cleanup external side effects for next test
+    pio_sm.gpio_reset_overrides();
+    pio_csr.wo(utra::rp_pio::SFR_IRQ0_INTE, 0);
+    pio_csr.wo(utra::rp_pio::SFR_IRQ1_INTE, 0);
+    pio_csr.wo(utra::rp_pio::SFR_SYNC_BYPASS, 0);
+
+    if passing {
+        report_api(0x05D1_600D);
+    } else {
+        report_api(0x05D1_DEAD);
+    }
+    assert!(passing);
+    passing
+}
