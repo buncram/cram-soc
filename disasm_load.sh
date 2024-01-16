9f424f84d90f26b4f8c0b67bb406154cdb2d4fff
buncram <cramium@kosagi.com>
2024-01-16 15:55:57 +0800

    add a missing helper script
    this generates code listings used by codezoom.py in a format
    it expects.
    
    it is automatically called by verilate.sh

---
+++ disasm_load.sh
@@ -0,0 +1,25 @@
+PREFIX="../xous-core"
+
+riscv-none-elf-objdump -h $PREFIX/target/riscv32imac-unknown-xous-elf/release/loader > listings/load.lst
+riscv-none-elf-nm -r --size-sort --print-size $PREFIX/target/riscv32imac-unknown-xous-elf/release/loader | rustfilt >> listings/load.lst
+riscv-none-elf-objdump $PREFIX/target/riscv32imac-unknown-xous-elf/release/loader -S -d | rustfilt >> listings/load.lst
+
+riscv-none-elf-objdump -h $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-kernel > listings/kernel.lst
+riscv-none-elf-nm -r --size-sort --print-size $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-kernel | rustfilt >> listings/kernel.lst
+riscv-none-elf-objdump $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-kernel -S -d | rustfilt >> listings/kernel.lst
+
+riscv-none-elf-objdump -h $PREFIX/target/riscv32imac-unknown-xous-elf/release/cram-console > listings/console.lst
+riscv-none-elf-nm -r --size-sort --print-size $PREFIX/target/riscv32imac-unknown-xous-elf/release/cram-console | rustfilt >> listings/console.lst
+riscv-none-elf-objdump $PREFIX/target/riscv32imac-unknown-xous-elf/release/cram-console -S -d | rustfilt >> listings/console.lst
+
+riscv-none-elf-objdump -h $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-ticktimer > listings/ticktimer.lst
+riscv-none-elf-nm -r --size-sort --print-size $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-ticktimer | rustfilt >> listings/ticktimer.lst
+riscv-none-elf-objdump $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-ticktimer -S -d | rustfilt >> listings/ticktimer.lst
+
+riscv-none-elf-objdump -h $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-log > listings/log.lst
+riscv-none-elf-nm -r --size-sort --print-size $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-log | rustfilt >> listings/log.lst
+riscv-none-elf-objdump $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-log -S -d | rustfilt >> listings/log.lst
+
+riscv-none-elf-objdump -h $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-names > listings/names.lst
+riscv-none-elf-nm -r --size-sort --print-size $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-names | rustfilt >> listings/names.lst
+riscv-none-elf-objdump $PREFIX/target/riscv32imac-unknown-xous-elf/release/xous-names -S -d | rustfilt >> listings/names.lst
