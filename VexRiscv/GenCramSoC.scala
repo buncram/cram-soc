7b2ef6efb95a5687ffc99a84b35e528bd34c0d18
buncram <cramium@kosagi.com>
2024-06-22 02:30:37 +0800

    add sramtrm pins to Vex

--- VexRiscv/GenCramSoC.scala
+++ VexRiscv/GenCramSoC.scala
@@ -62,12 +62,14 @@ object GenCramSoC{
       override def impl(pc: PhaseContext): Unit = {
         val topPatch = pc.topLevel rework new AreaRoot{
           val CMBIST, CMATPG = in Bool()
+          val sramtrm = in UInt(3 bits)
         }
         pc.walkComponents{
           case c : Ram_1w_1rs => {
             c.rework {
               topPatch.CMBIST.pull(propagateName = true)
               topPatch.CMATPG.pull(propagateName = true)
+              topPatch.sramtrm.pull(propagateName = true)
             }
             // c.genericElements.clear()
             c.addGeneric("ramname", s"RAM_DP_${c.wordCount}_${c.wordWidth}")
