ad514886a6471b8932c519378e743320f855068f
bunnie <bunnie@kosagi.com>
2024-11-26 10:54:15 +0800

    add bist port to register_ram option path

--- deps/bio/bio_bdma.sv
+++ deps/bio/bio_bdma.sv
@@ -1783,6 +1783,7 @@ module bio_bdma #(
                 .q(ram_rd_data[j]),
                 .cmbist(cmbist),
                 .cmatpg(cmatpg),
+                .rbs(rbif_bioram1kx32[j]),
                 .sramtrm(sramtrm)
             );
             always_ff @(posedge aclk) begin
