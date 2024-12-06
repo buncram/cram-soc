8ca49b5d33d6fe55314ad91fc6f819176dd11e0a
bunnie <bunnie@kosagi.com>
2024-12-06 19:29:35 +0800

    reflect manual edit in bio tb

--- candidate/stage.sh
+++ candidate/stage.sh
@@ -25,7 +25,7 @@ cp ../sim_support/mbox_client.v ./mbox/rtl/
 cp ../sim_support/ram_1w_1ra.v ./libs/
 cp ../sim_support/ram_1w_1rs.v ./libs/
 cp ../sim_support/fdre_cosim.v ./libs/
-cp ../sim_support/bio_tb.v ./tb/
+# cp ../sim_support/bio_tb.v ./tb/ # this has diverged in production due to manual edits to code
 cp ../VexRiscv/VexRiscv_CramSoC.v ./libs/
 cp ../VexRiscv/memory_AesZknPlugin_rom_storage_Rom_1rs.v ./libs/
 cp ../build/gateware/cram_axi.v .
