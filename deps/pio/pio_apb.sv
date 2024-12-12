2f2ecb41cc0f85942b9febd3eae8e3afe9b899d6
buncram <cramium@kosagi.com>
2024-12-12 13:50:16 +0800

    update copyright notice

--- deps/pio/pio_apb.sv
+++ deps/pio/pio_apb.sv
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Cramium Labs, Inc.
+// SPDX-FileCopyrightText: 2024 CrossBar, Inc.
 // SPDX-FileCopyrightText: 2022 Lawrie Griffiths
 // SPDX-License-Identifier: BSD-2-Clause
 
